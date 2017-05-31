f43513a7d8be802862bd47b43b0d53a2206be506
jack-freiermuth <jack.freiermuth@gmail.com>
2017-05-30 20:17:26 -0500

    start changes

--- index.php
+++ index.php
@@ -153,6 +153,8 @@
 		</div>
 	</div>
 </section>
+
+
 <section class="skill-holder"><!--skills-->
 
 	<?php
@@ -379,6 +381,25 @@
 	</div>
 </section> -->
 <!--end of Testimonial-->
+
+<section><!--Projects section-->
+	<div class="container">
+		<div class="row">
+			<h2 class="subtitle">Projects</h2>
+			<div class="card card-inverse">
+				<img class="card-img" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22454%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20454%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15c577f0185%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A23pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15c577f0185%22%3E%3Crect%20width%3D%22454%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22168.1796875%22%20y%3D%22100.3171875%22%3E454x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Project 1 Image">
+				<!-- <img class="card-img" src="images/snow_monkey.jpg" alt="Card image"> -->
+				<div class="card-img-overlay">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!--/Projects section-->
+
+
 
 <footer id="contact"><!-- footer -->
 	<div class="container custom-container">
